7a9ed0544ad10294d767e683837b4cb3474767b1
panji.febriyanto <panji.febriyanto@ptbsp.com>
2024-09-20 11:09:14 +0700

    update

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ import asyncio
 from apscheduler.schedulers.background import BackgroundScheduler
 from utilities.instagram import schedule_get_follower
 
-from routes.etl import data_transaksi, chat_wa, sum_transaksi, sum_average_sales, sum_customer, sum_model, sum_region, sum_sales_trend, sum_sales_trend_pertanggal, sum_store, sum_top_produk, sum_wa, sum_customer_follower
-
 from database import engine, database
 from consumer.transaction_consumer import start_consumer  
 import threading
@@ -30,40 +28,9 @@ app = FastAPI()
 async def root():
     return {"message": "TLE API version 1.0"}
 
-scheduler = True
-def run_data_transaksi():
-    asyncio.run(data_transaksi(scheduler))
-
-def run_chat_wa():
-    asyncio.run(chat_wa(scheduler))
-
-def run_data_summary():
-    asyncio.run(sum_transaksi())
-    asyncio.run(sum_average_sales())
-    asyncio.run(sum_customer())
-    asyncio.run(sum_model())
-    asyncio.run(sum_region())
-    asyncio.run(sum_sales_trend())
-    asyncio.run(sum_sales_trend_pertanggal())
-    asyncio.run(sum_store())
-    asyncio.run(sum_top_produk())
-    asyncio.run(sum_wa())
-    asyncio.run(sum_customer_follower())
-
-def run_get_follower():
-    asyncio.run(schedule_get_follower())
-
 @app.on_event("startup")
 async def startup():
     await database.connect()
-    
-    # jalanin scheduler    
-    # sched = BackgroundScheduler()
-    # sched.add_job(run_data_transaksi, trigger='interval', minutes=5)
-    # sched.add_job(run_chat_wa, trigger='interval', seconds=60)
-    # sched.add_job(run_data_summary, trigger='interval', minutes=10)
-    # sched.add_job(run_get_follower, trigger='interval', minutes=60)
-    # sched.start()
 
     run_consumer_in_background()
 
